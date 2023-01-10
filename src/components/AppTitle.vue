1e6298627c54236126b6c593078e528477cc29d4
dunzolo <rossidavide108@gmail.com>
2023-01-10 15:06:16 +0100

    added apptitle components

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+        return {
+            title: "La mia prima app con Vite!",
+        }
+    },
+}
+</script>
+
+<template lang="">
+    <div>
+        {{ title }}
+    </div>
+</template>
+<style lang="">
+    
+</style>
