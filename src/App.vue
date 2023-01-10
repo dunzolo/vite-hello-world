af4132c6198d8410a8bd5fb0cb94b3386f12fac8
dunzolo <rossidavide108@gmail.com>
2023-01-10 15:25:19 +0100

    added img jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script>
 import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
+
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   }
 }
 </script>
@@ -10,6 +13,7 @@ export default {
 <template>
   <div>
     <AppTitle></AppTitle>
+    <AppHero></AppHero>
   </div>
 </template>
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppHero',
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template lang="">
+    <div>
+        <img src="/Avengers-Age-of-Ultron.jpg" alt="">
+    </div>
+</template>
+<style lang="">
+    
+</style>
